9c90fb9b61f4c326509e25656f0885975cfb1e67
wisegirl139-sudo <wisegirl139@gmail.com>
2026-09-12 21:32:43 +0100

    the only thing i want for my birthday and everything is for all my items to be aligned properly at first try

--- secpage/last/last.js
+++ secpage/last/last.js
@@ -25,7 +25,7 @@ const pTense = localStorage.getItem("pTense");
 const jobType = localStorage.getItem("jobType");
 const transpoType = localStorage.getItem("transpoType");
 document.getElementById("mt").innerHTML = `
-${nAme}'s perfect holiday plan!! First thing first, its time to pack a ${nNoun}, and obviously you have to do it very ${adj}ly.
+${nAme}'s PERFECT holiDAY!! First thing first, its time to pack a ${nNoun}, and obviously you have to do it very ${adj}ly.
 Once that's done, you're ${vErb}ing out the door with ${things} in hand. Travelling at ${aGe} is crazy cool, but before you go, you stop to admire the ${Anml} just one last time before heading to ${dEstination}.
 You're already planning on going ${cAsia} next time, and hopefully by then you'll be a ${jobType}. But for now, you've got your ${cOlour} suitcase in one hand and some brilliant ${food} in the other as you make your way to the ${transpoType}.
 And just like that youre off on youre new adventure, enjoy your trip!!!
